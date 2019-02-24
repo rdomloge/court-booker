af0b482a71c2b78831b80aeb6ab3906e83486f5e
Ramsay Domloge <ramsay.domloge@bca.com>
2019-02-24 01:39:14 +0000

    Added an aggregation method

--- src/main/java/com/domloge/iot/iotendpoint/FlowReadingRepository.java
+++ src/main/java/com/domloge/iot/iotendpoint/FlowReadingRepository.java
@@ -3,6 +3,7 @@ package com.domloge.iot.iotendpoint;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -20,4 +21,10 @@ public interface FlowReadingRepository extends PagingAndSortingRepository<FlowRe
 			@Param("time")Date from);
 	
 	List<FlowReading> findTop50By();
+	
+	
+	@Query(value = "select id, source, sum(flow_lpm), time, hostname "
+			+ "from flow_reading fr where time > '2019-02-01'"
+			+ "group by time", nativeQuery = true)
+	List<FlowReading> dailyAggregation();
 }
