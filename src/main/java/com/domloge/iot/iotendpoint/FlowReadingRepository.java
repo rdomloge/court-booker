4a60e08d6612d7c52a7db4fea2e8faf139be231b
Ramsay Domloge <ramsay.domloge@bca.com>
2019-03-02 23:16:33 +0000

    Added some code for daily totals

--- src/main/java/com/domloge/iot/iotendpoint/FlowReadingRepository.java
+++ src/main/java/com/domloge/iot/iotendpoint/FlowReadingRepository.java
@@ -3,12 +3,12 @@ package com.domloge.iot.iotendpoint;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource(path = "flowReadings")
@@ -17,14 +17,40 @@ public interface FlowReadingRepository extends CrudRepository<FlowReading, Integ
 	List<FlowReading> findByTimeGreaterThanEqual(
 			@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm")
 			@Param("time")Date from);
+	
+	@Query(value =	"SELECT rownum() as ID, * FROM\n" + 
+			"(\n" + 
+			"SELECT \n" + 
+			"  CAST(time as DATE) as time, \n" + 
+			"  source, \n" + 
+			"  hostname,\n" + 
+			"  SUM(flow_lpm) as flow_lpm\n" + 
+			"FROM flow_reading \n" + 
+			"WHERE time >= :time\n" + 
+			"GROUP BY time,hostname\n" + 
+			"ORDER BY time\n" + 
+			")",
+			nativeQuery=true)
+	List<FlowReading> findDailyAmountByTimeGreaterThanEqualNative(
+			@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm")
+			@Param("time")Date from);
+	
+//	SELECT 
+//	    DATE(created_at) AS create_date,
+//	    COUNT(id) AS total
+//	FROM
+//	    data_table
+//	GROUP BY 
+//	    DATE(created_at)
+	
 
-	List<FlowReading> readAllByFlowLpmGreaterThan(Float lpm);
-
-	List<FlowReading> readAllByFlowLpmGreaterThanAndTimeGreaterThanEqual(
-		Float lpm, 
-		@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm") 
-		@Param("time")
-		Date from);
+//	List<FlowReading> readAllByFlowLpmGreaterThan(Float lpm);
+//
+//	List<FlowReading> readAllByFlowLpmGreaterThanAndTimeGreaterThanEqual(
+//		Float lpm, 
+//		@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm") 
+//		@Param("time")
+//		Date from);
 
 	@Modifying
 	@Transactional
