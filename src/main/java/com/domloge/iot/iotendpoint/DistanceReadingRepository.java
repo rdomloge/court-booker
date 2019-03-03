a1d69ca592debd5345f9f1a7930c1d6152b71b11
Ramsay Domloge <ramsay.domloge@bca.com>
2019-03-03 00:08:45 +0000

    Added daily average

--- src/main/java/com/domloge/iot/iotendpoint/DistanceReadingRepository.java
+++ src/main/java/com/domloge/iot/iotendpoint/DistanceReadingRepository.java
@@ -3,6 +3,7 @@ package com.domloge.iot.iotendpoint;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,13 +12,25 @@ import org.springframework.format.annotation.DateTimeFormat;
 @RepositoryRestResource(path = "distanceReadings")
 public interface DistanceReadingRepository extends PagingAndSortingRepository<DistanceReading, Integer> {
 
-	List<DistanceReading> findBySource(String source);
-	
-	List<DistanceReading> findByHostname(String hostname);
 	
 	List<DistanceReading> findByTimeGreaterThanEqual(
 			@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm")
 			@Param("time")Date from);
 	
-	List<DistanceReading> findTop50By();
+	@Query(value =	"SELECT rownum() as ID, * FROM\n" + 
+			"(\n" + 
+			"SELECT \n" + 
+			"  CAST(time as DATE) as time, \n" + 
+			"  source, \n" + 
+			"  hostname,\n" + 
+			"  AVG(distance_cm) as distance_cm\n" + 
+			"FROM distance_reading \n" + 
+			"WHERE time >= :time\n" + 
+			"GROUP BY time,hostname\n" + 
+			"ORDER BY time\n" + 
+			")",
+			nativeQuery=true)
+	List<DistanceReading> findDailyAverageByTimeGreaterThanEqualNative(
+			@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm")
+			@Param("time")Date from);
 }
