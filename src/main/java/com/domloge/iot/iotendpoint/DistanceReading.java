8afaf97e5e65519c16a08e0d89ce6721a20a905f
Ramsay Domloge <ramsay.domloge@bca.com>
2019-03-03 00:39:56 +0000

    Added indexes

--- src/main/java/com/domloge/iot/iotendpoint/DistanceReading.java
+++ src/main/java/com/domloge/iot/iotendpoint/DistanceReading.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -15,6 +17,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 @Entity
+@Table(indexes = {@Index(name="time_index", columnList="time", unique=false)})
 public class DistanceReading {
 
 	@Id
