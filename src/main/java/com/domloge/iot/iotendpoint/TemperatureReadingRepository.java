8352321b9542418a8b17dcf1fcd95d6854e8e1db
Ramsay Domloge <ramsay.domloge@bca.com>
2019-03-04 23:47:36 +0000

    More coolness

--- src/main/java/com/domloge/iot/iotendpoint/TemperatureReadingRepository.java
+++ src/main/java/com/domloge/iot/iotendpoint/TemperatureReadingRepository.java
@@ -3,6 +3,7 @@ package com.domloge.iot.iotendpoint;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,14 +12,52 @@ import org.springframework.format.annotation.DateTimeFormat;
 @RepositoryRestResource(path = "temperatureReadings")
 public interface TemperatureReadingRepository extends PagingAndSortingRepository<TemperatureReading, Integer> {
 
-	List<TemperatureReading> findBySource(String source);
-	
-	List<TemperatureReading> findByHostname(String hostname);
-	
 	List<TemperatureReading> findByTimeGreaterThan(
 			@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm")
 			@Param("time")Date from);
 	
-	List<TemperatureReading> findTop50By();
+	@Query(value="SELECT rownum() as ID, * FROM\n" + 
+			"(\n" +
+			"SELECT \n" + 
+			"  CAST(time as DATE) as time, \n" + 
+			"  source, \n" + 
+			"  hostname,\n" + 
+			"  AVG(temp) as temp\n" + 
+			"FROM temperature_reading \n" + 
+			"WHERE time >= TODAY()\n" + 
+			"GROUP BY time,hostname\n" + 
+			"ORDER BY time\n" + 
+			")", 
+			nativeQuery=true)
+	TemperatureReading findAverageTempToday();
 	
+	@Query(value="SELECT rownum() as ID, * FROM\n" + 
+			"(\n" +
+			"SELECT \n" + 
+			"  CAST(time as DATE) as time, \n" + 
+			"  source, \n" + 
+			"  hostname,\n" + 
+			"  MIN(temp) as temp\n" + 
+			"FROM temperature_reading \n" + 
+			"WHERE time >= TODAY()\n" + 
+			"GROUP BY time,hostname\n" + 
+			"ORDER BY time\n" + 
+			")", 
+			nativeQuery=true)
+	TemperatureReading findMinimumTempToday();
+	
+	@Query(value="SELECT rownum() as ID, * FROM\n" + 
+			"(\n" +
+			"SELECT \n" + 
+			"  CAST(time as DATE) as time, \n" + 
+			"  source, \n" + 
+			"  hostname,\n" + 
+			"  MAX(temp) as temp\n" + 
+			"FROM temperature_reading \n" + 
+			"WHERE time >= TODAY()\n" + 
+			"GROUP BY time,hostname\n" + 
+			"ORDER BY time\n" + 
+			")", 
+			nativeQuery=true)
+	TemperatureReading findMaximumTempToday();
 }
