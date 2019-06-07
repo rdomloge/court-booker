14470fc8d10574b806544ddc185cf97944f76b3b
Ramsay Domloge <ramsay.domloge@bca.com>
2019-06-07 23:24:23 +0100

    Tried another format

--- src/main/java/com/domloge/iot/iotendpoint/TemperatureReadingRepository.java
+++ src/main/java/com/domloge/iot/iotendpoint/TemperatureReadingRepository.java
@@ -8,12 +8,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @RepositoryRestResource(path = "temperatureReadings")
 public interface TemperatureReadingRepository extends PagingAndSortingRepository<TemperatureReading, Integer> {
 
 	List<TemperatureReading> findByTimeGreaterThan(
-			@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm.SSSZ")
+			@DateTimeFormat(iso = ISO.DATE_TIME)
 			@Param("time")Date from);
 	
 	@Query(value="SELECT rownum() as ID, * FROM\n" + 
